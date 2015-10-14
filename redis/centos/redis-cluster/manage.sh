01cf5472cf45e08f59c3dc520c07d1c52f5f4101
raosungang <raosungang@hotmail.com>
2015-10-14 11:51:54 +0800

    change redis-cluster

---
+++ redis/centos/redis-cluster/manage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /data
+export IPADDR=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+echo $IPADDR
+#sed -i "s/6379/$redis_port/g" redis.conf
+#sed -i 's/daemonize no/daemonize yes/' redis.conf
+sed -i 's/appendonly no/appendonly yes/' redis.conf
+sed -i 's/# cluster-enabled yes/cluster-enabled yes/g' redis.conf
+sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/g' redis.conf
+sed -i 's/# cluster-config-file nodes.*/cluster-config-file nodes-'$IPADDR'.conf/g' redis.conf
+redis-server redis.conf
+#while 1;do
+#        sleep 30;
+#done
+~                    
